74c435d76eebe1868f4d948552b2b9c7ebcdb3b5
seokcheon <my980519@gmail.com>
2023-12-22 17:49:40 +0900

    modify

---
+++ docs/quests/jobplanet_login_selector docs.py
@@ -0,0 +1,11 @@
+#주소
+# https://www.jobplanet.co.kr/users/sign_in?_nav=gb
+
+#email
+#user_email
+
+#password
+##user_password
+
+# login_button
+# div > section.section_email.er_r > fieldset > button
